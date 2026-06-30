2ebf1cc0a40c3a7b23276518ea022c716b541a8b
muhino1 <muhino1@unlv.nevada.edu>
2026-06-29 20:47:00 -0700

    Update evaluate.c

--- user/evaluate.c
+++ user/evaluate.c
@@ -22,5 +22,62 @@ int main(void)
 	//TODO: Write your implementation here desrcibed in the document
     //TODO: Evaluate your metrics here, after the processes have completed their work
 
+  int t0 = uptime();
+
+    for(int i = 0; i < 3; i++){
+        int pid = fork();
+        if(pid == 0){      // child process
+            rude(rudeness[i]);
+            int my_start = uptime();
+
+            if(i == 0){                // rudeness 5: sum
+                int sum = 0;
+                for(int j = 0; j < ARR_SIZE; j++)
+                    sum += arr[j];
+                printf("Sum: %d\n", sum);
+            } else if(i == 1){         // rudeness 15: normalize
+                int min = arr[0], max = arr[0];
+                for(int j = 1; j < ARR_SIZE; j++){
+                    if(arr[j] < min) min = arr[j];
+                    if(arr[j] > max) max = arr[j];
+                }
+                printf("Normalized:\n");
+                for(int j = 0; j < ARR_SIZE; j++){
+                    int num = arr[j] - min;
+                    int denom = max - min;
+                    printf("%d/%d ", num, denom); // fraction since no floats
+                }
+                printf("\n");
+            } else {                // rudeness 30: bubble sort
+                for(int j = 0; j < ARR_SIZE-1; j++){
+                    for(int k = 0; k < ARR_SIZE-j-1; k++){
+                        if(arr[k] > arr[k+1]){
+                            int tmp = arr[k];
+                            arr[k] = arr[k+1];
+                            arr[k+1] = tmp;
+                        }
+                    }
+                }
+                printf("Sorted:\n");
+                for(int j = 0; j < ARR_SIZE; j++)
+                    printf("%d ", arr[j]);
+                printf("\n");
+            }
+
+            int my_end = uptime();
+            printf("Child %d (rudeness %d) turnaround: %d ticks\n", i, rudeness[i], my_end - my_start);
+            exit(0);
+        } else {
+            forkpids[i] = pid;
+        }
+    }
+
+    for(int i = 0; i < 3; i++){
+        wait(0);
+    }
+
+    int t_end = uptime();
+    printf("Total elapsed: %d ticks\n", t_end - t0);
+
 	free(arr);
 }
