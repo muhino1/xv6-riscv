47c90f221b1692be99fe2ed75223b8cf6e1c1a0a
muhino1 <muhino1@unlv.nevada.edu>
2026-06-29 19:38:24 -0700

    Update syscall.c

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -102,6 +102,7 @@ extern uint64 sys_unlink(void);
 extern uint64 sys_link(void);
 extern uint64 sys_mkdir(void);
 extern uint64 sys_close(void);
+extern uint64 sys_rude(void);
 
 // An array mapping syscall numbers from syscall.h
 // to the function that handles the system call.
